ea197836d4cb1702bab1637099acac2f0d938ed0
Rico Sta. Cruz <rico@ambiescent.com>
2011-03-30 08:16:00 +0800

    + css_mtime_for helper

--- lib/sinatra/support/csssupport.rb
+++ lib/sinatra/support/csssupport.rb
@@ -41,6 +41,16 @@
 #   register Sinatra::CssSupport
 #   Main.set :css_aggressive_mtime, false
 #
+# == Helpers
+#
+# {Helpers#css_mtime_for css_mtime_for} - Returns the last modified time for
+# the given stylesheet.
+#
+#   <link rel="stylesheet" type="text/css" href="/css/style.css?<%= css_mtime_for(root('app/css/style.css')) %>">
+#
+# This takes into account the `css_aggressive_mtime` setting.
+# Perfect for nocache hacks, like above.
+#
 # == Settings
 #
 # [+css_max_age+]            The maximum time (in seconds) a browser
@@ -48,11 +58,13 @@
 #                            (default: 30 days)
 # [+css_aggressive_mtime+]   If true, ask browsers to recache all CSS files
 #                            when one is updated. Default: true
-#                     
+#
 module Sinatra::CssSupport
   def self.registered(app)
     app.set :css_max_age, app.development? ? 0 : 86400*30
     app.set :css_aggressive_mtime, true
+
+    app.helpers Helpers
   end
 
   def serve_css(url_prefix, options={})
@@ -63,13 +75,7 @@ module Sinatra::CssSupport
       fname = Dir[File.join(prefix, "#{name}.{css,scss,less}")].first  or pass
 
       content_type :css, :charset => 'utf-8'
-
-      if settings.css_aggressive_mtime
-        last_modified Dir[prefix, "**/*"].map { |f| File.mtime(f).to_i }.max
-      else
-        last_modified File.mtime(fname)
-      end
-
+      last_modified css_mtime_for(prefix, fname)
       cache_control :public, :must_revalidate, :max_age => settings.css_max_age
 
       if fname =~ /\.scss$/
@@ -83,5 +89,15 @@ module Sinatra::CssSupport
       end
     end
   end
-end
 
+  module Helpers
+    def css_mtime_for(prefix=nil, fname)
+      if settings.css_aggressive_mtime
+        prefix ||= File.basename(fname)
+        Dir[prefix, "**/*"].map { |f| File.mtime(f).to_i }.max
+      else
+        File.mtime(fname).to_i
+      end
+    end
+  end
+end
