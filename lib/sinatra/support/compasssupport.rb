1ced0d6070d12902c092b13f78875a107fb2c5d7
Rico Sta. Cruz <rico@ambiescent.com>
2011-05-27 12:31:33 +0800

    ~ Revise CompassSupport to not use config files (instead just use Compass.configuration)

--- lib/sinatra/support/compasssupport.rb
+++ lib/sinatra/support/compasssupport.rb
@@ -20,10 +20,6 @@
 # more-complicated example:
 #
 #   class Main
-#     # This line below is optional (it defaults to `config/compass.config`;
-#     # in fact, it doesn't need to exist):
-#     set :compass_config_file, File.join(root, 'config/compass.conf')
-#
 #     register Sinatra::CompassSupport
 #
 #     get '/' do
@@ -35,10 +31,18 @@
 #
 #   @import 'compass/css3';
 #
-# Then copy +compass.config+ into +config/compass.conf+ from
-# https://github.com/chriseppstein/compass-sinatra/blob/master/config/compass.config
-#
 # == Caveats
+#
+# When When using with {Sinatra::CssSupport}, you may need to change Compass's 
+# +sass_dir+ config to where you're serving CSS files from.
+#
+#   path = File.join(app.root, 'css')
+#
+#   serve_css '/css', from: path
+#
+#   Compass.configuration do |c|
+#     c.sass_dir = path
+#   end
 #
 # If you are getting errors about Sass functions, you may need to upgrade your
 # HAML and Sass gems.
@@ -58,21 +62,14 @@ module Sinatra::CompassSupport
     require 'compass'
 
     add_compass_engine_options app
-    set_default_compass_config app
-    load_compass_config app
   end
 
 private
-  def self.set_default_compass_config(app)
-    unless app.respond_to?(:compass_config_file)
-      app.set :compass_config_file, File.join(*[app.root, 'config', 'compass.config'].compact)
+  def self.lol(app)
+    Compass.configuration do |c|
+      c.project_path = app.root
+      c.sass_dir     = app.views
     end
-  end
-
-  def self.load_compass_config(app)
-    file = app.compass_config_file
-
-    Compass.add_project_configuration(file)  if File.exists?(file)
   end
 
   def self.add_compass_engine_options(app)
